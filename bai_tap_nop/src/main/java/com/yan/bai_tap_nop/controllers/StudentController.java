12349979d1d5878aff7ae810f8895449d1cc332c
Yami An <yamiannephilim@gmail.com>
2022-07-30 15:05:11 +0700

    yan

--- bai_tap_nop/src/main/java/com/yan/bai_tap_nop/controllers/StudentController.java
+++ bai_tap_nop/src/main/java/com/yan/bai_tap_nop/controllers/StudentController.java
@@ -10,20 +10,20 @@ import com.yan.bai_tap_nop.models.*;
 @RequestMapping("/student")
 public class StudentController {
     //#region Fields
-    private List<Student> students = new ArrayList<>();
+    private List<Student> mStudents = new ArrayList<>();
     //#endregion
 
     //#region Constructors
     public StudentController(){
-        students.add(new Student("Nguyễn Văn A", 20));
-        students.add(new Student("Trần Văn B", 21));
+        mStudents.add(new Student("Nguyễn Văn A", 20));
+        mStudents.add(new Student("Trần Văn B", 21));
     }
     //#endregion
 
     //#region Methods
     @GetMapping("/")
     public List<Student> getAllStudents() {
-        return students;
+        return mStudents;
     }
 
     @GetMapping("/add")
@@ -31,8 +31,8 @@ public class StudentController {
         Student student = new Student();
         student.setName(name);
         student.setAge(age);
-        students.add(student);
-        return students;
+        mStudents.add(student);
+        return mStudents;
     }
 
     @GetMapping("/add/{name}/{age}")
@@ -40,14 +40,14 @@ public class StudentController {
         Student student = new Student();
         student.setName(name);
         student.setAge(age);
-        students.add(student);
-        return students;
+        mStudents.add(student);
+        return mStudents;
     }
 
     @PostMapping(value = "add", consumes = "application/json")
     public List<Student> add(@RequestBody Student student) {
-        students.add(student);
-        return students;
+        mStudents.add(student);
+        return mStudents;
     }
     //#endregion
 }
